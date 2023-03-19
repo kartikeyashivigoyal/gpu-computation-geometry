d3b2f10401ece71aa05c1b78482068028e763158
Kartikeya Goyal <kgoyal@nvidia.com>
2023-03-19 12:49:36 -0700

    Point class

---
+++ point.cc
@@ -0,0 +1,9 @@
+
+#include "point.h"
+
+typedef Point2D<float, float> Point2Df;
+
+int main()
+{
+    auto p = Point2Df(5.0, 5.5);
+}
---
+++ point.h
@@ -0,0 +1,19 @@
+// Point2D
+/// @brief Point represented by two co-ordinates
+/// @tparam T type of 1st co-ordinate
+/// @tparam V type of 2nd co-ordinate
+template <typename T, typename V>
+class Point2D
+{
+public:
+    T x;
+    V y;
+
+    Point2D(T a, V b)
+    {
+        x = a;
+        y = b;
+    };
+
+    
+};
